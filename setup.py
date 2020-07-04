d3ccd9d5829b43241465baab0a3854ea6f89ff10
alexcatin <koshin_alex@mail.ru>
2020-07-04 18:45:51 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name="mycalc",
+    version="0.0.1",
+    description="A small example package with some arithmetics",
+    packages=find_packages(),
+    include_package_data=True,
+    author="AlexCatIn",
+    author_email="koshin_alex@mail.ru",
+    url="https://github.com/alexcatin/mycalc",
+)
